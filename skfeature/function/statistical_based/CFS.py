8eff7c64546a18a7bb4ff128ae08372a47ceb3dd
Ivan Letteri <ivan.informatica09@gmail.com>
2020-03-21 19:41:45 +0100

    Update CFS.py

--- skfeature/function/statistical_based/CFS.py
+++ skfeature/function/statistical_based/CFS.py
@@ -84,5 +84,50 @@ def cfs(X, y):
                     if M[len(M)-3] <= M[len(M)-4]:
                         if M[len(M)-4] <= M[len(M)-5]:
                             break
-    return np.array(F), np.array(Merits)
+    return np.array(F)
 
+def cfsMscore(X, y):
+    """
+    This function uses a correlation based heuristic to evaluate the worth of features which is called CFS
+    Input
+    -----
+    X: {numpy array}, shape (n_samples, n_features)
+        input data
+    y: {numpy array}, shape (n_samples,)
+        input class labels
+    Output
+    ------
+    F: {numpy array}
+        index of selected features
+    Reference
+    ---------
+    Zhao, Zheng et al. "Advancing Feature Selection Research - ASU Feature Selection Repository" 2010.
+    """
+
+    n_samples, n_features = X.shape
+    F = []
+    # M stores the merit values
+    M = []
+    Merits = []
+    while True:
+        merit = -100000000000
+        idx = -1
+        for i in range(n_features):
+            if i not in F:
+                F.append(i)
+                # calculate the merit of current selected features
+                t = merit_calculation(X[:, F], y)
+                Merits.append(t)
+                if t > merit:
+                    merit = t
+                    idx = i
+                F.pop()
+        F.append(idx)
+        M.append(merit)
+        if len(M) > 5:
+            if M[len(M)-1] <= M[len(M)-2]:
+                if M[len(M)-2] <= M[len(M)-3]:
+                    if M[len(M)-3] <= M[len(M)-4]:
+                        if M[len(M)-4] <= M[len(M)-5]:
+                            break
+return np.array(Merits)
