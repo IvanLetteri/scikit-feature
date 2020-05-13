7fc7f15ad6bb255e60e09899317d0db7631c1815
Ivan Letteri <ivan.informatica09@gmail.com>
2020-05-13 19:43:55 +0200

    Update MRMR.py

--- skfeature/function/information_theoretical_based/MRMR.py
+++ skfeature/function/information_theoretical_based/MRMR.py
@@ -23,6 +23,7 @@ def mrmr(X, y, **kwargs):
         corresponding objective function value of selected features
     MIfy: {numpy array}, shape: (n_features,)
         corresponding mutual information between selected features and response
+    dictFeatJcmi: ..........
 
     Reference
     ---------
@@ -30,7 +31,7 @@ def mrmr(X, y, **kwargs):
     """
     if 'n_selected_features' in kwargs.keys():
         n_selected_features = kwargs['n_selected_features']
-        F, J_CMI, MIfy = LCSI.lcsi(X, y, gamma=0, function_name='MRMR', n_selected_features=n_selected_features)
+        F, J_CMI, MIfy, dictFeatJcmi = LCSI.lcsi(X, y, gamma=0, function_name='MRMR', n_selected_features=n_selected_features)
     else:
-        F, J_CMI, MIfy = LCSI.lcsi(X, y, gamma=0, function_name='MRMR')
-    return F, J_CMI, MIfy
+        F, J_CMI, MIfy, dictFeatJcmi = LCSI.lcsi(X, y, gamma=0, function_name='MRMR')
+    return F, J_CMI, MIfy, dictFeatJcmi
