eee41ad0fcc1da0c07a583e4b05d67041d71c809
Ivan Letteri <ivan.informatica09@gmail.com>
2020-05-15 18:28:44 +0200

    Update CIFE.py

--- skfeature/function/information_theoretical_based/CIFE.py
+++ skfeature/function/information_theoretical_based/CIFE.py
@@ -29,9 +29,10 @@ def cife(X, y, **kwargs):
     Brown, Gavin et al. "Conditional Likelihood Maximisation: A Unifying Framework for Information Theoretic Feature Selection." JMLR 2012.
     """
     
-    if 'n_selected_features' in kwargs.keys():
+    if('n_selected_features' in kwargs.keys() AND 'dict_features' in kwargs.keys()):
         n_selected_features = kwargs['n_selected_features']
-        F, J_CMI, MIfy, dictFeatJcmi = LCSI.lcsi(X, y, beta=1, gamma=1, n_selected_features=n_selected_features)
+        dictOfHeader = kwargs['dict_features']
+        F, J_CMI, MIfy, dictFeatJcmi = LCSI.lcsi(X, y, beta=1, gamma=1, n_selected_features=n_selected_features, dict_features=dictOfHeader)
     else:
         F, J_CMI, MIfy, dictFeatJcmi = LCSI.lcsi(X, y, beta=1, gamma=1)
     return F, J_CMI, MIfy, dictFeatJcmi
